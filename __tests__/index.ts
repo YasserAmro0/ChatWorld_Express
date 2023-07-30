c544214ef3f1284f29d145b8116fb2a7b24f1e13
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-07-30 16:04:15 +0300

    fix: test file

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,16 +1,13 @@
-function add(a : number, b : number) {
+const add = (a, b) => {
     return a + b;
-}
+};
 
-// Write the test using Jest
 test('should return the sum of 1 + 1', () => {
-    // Arrange
     const num1 = 1;
     const num2 = 1;
 
-    // Act
+    
     const result = add(num1, num2);
 
-    // Assert
     expect(result).toBe(2);
 });
