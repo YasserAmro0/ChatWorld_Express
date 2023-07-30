4da8e3b0a7d169c00c11f84f23b3aa14fa8cf2a9
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-07-30 15:33:30 +0300

    build: add to test to push code

---
+++ __tests__/index.ts
@@ -0,0 +1,16 @@
+function add(a:number, b:number) {
+    return a + b;
+}
+
+// Write the test using Jest
+test('should return the sum of 1 + 1', () => {
+    // Arrange
+    const num1 = 1;
+    const num2 = 1;
+
+    // Act
+    const result = add(num1, num2);
+
+    // Assert
+    expect(result).toBe(2);
+});
